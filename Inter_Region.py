67f0db0ee537fce6d88daa857b32fc41402947f9
ariel rubanenko <ariel@algonomi.com>
2026-09-07 15:58:23 +0300

    Inter_Region new function

--- Inter_Region.py
+++ Inter_Region.py
@@ -1,25 +1,26 @@
-def inter_continent(country_data):
+def inter_region(country_data):
     import seaborn as sns
     import statistics as stt
     import matplotlib.pyplot as plt
-    #libs_and_modules
     figs = []
     df = country_data
+
     exclude_col = ["gdp","rel","population","surface_area"]
 
     DivideByPop = ['tourists','area','co2_emissions']
     for col in df.keys():
 
         if col not in exclude_col and  isinstance(df.loc[df.index[0],col],float):
+            grouped_df = df.groupby('region')[col].mean()
             fig, ax = plt.subplots(figsize=(8, 5))
 
             lb = col
             if col in DivideByPop:
                 df['rel']= df[col]/df['population']
-                sns.barplot(data=df, x='rel',  hue='Continent',ax=ax)
                 lb = lb + ' per capita'
-                plt.xlabel(lb)
-            else:
-                sns.barplot(data=df, x=col,  hue='Continent',ax=ax)
+
+
+            sns.barplot(x=grouped_df, y=grouped_df.index, ax=ax)
+            plt.xlabel(lb)
             figs.append(fig)
     return figs
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from manage_app_interface import *
 from Intra_country_functions import *
 from Inter_Country import inter_country
 from Inter_Continent import inter_continent
-
+from Inter_Region import inter_region
 # Default values for configurable parameters
 row_dropna_threshold_factor = 0.9 # The relation between the number of non empty cells to the total number of cells in a row
 
@@ -31,11 +31,18 @@ if analysis_type == 'intra_country':
 
 # To be integrated into the manage_app_interface - Temporarily here for inspection
 #get_country_id_data(country_data.loc[['Canada']],country_all_columns)
+
+
 MyCountry = st.selectbox('Select Country',country_data.index)
 figs = inter_country(country_data,MyCountry,compareTo='World')
 for fig in figs:
     st.pyplot(fig)
 
 figs= inter_continent(country_data)
+for fig in figs:
+    st.pyplot(fig)
+
+
+figs= inter_region(country_data)
 for fig in figs:
     st.pyplot(fig)
