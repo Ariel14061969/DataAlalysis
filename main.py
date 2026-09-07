cf061bb772589d95db785ebb640d2ff780ef9596
sinaylior <sinaylior@gmail.com>
2026-09-08 00:02:31 +0300

    Applied the following updates: 1. main -    * Integrated new functions from Ariel    * Support additional log file created by      the Intra_country_functions.py
    2. Intra_country_functions.py -
       * Handled scenario of plotting in case some of the relevant cells have missing data.
       * Added log file "intra_country_function_logs.txt"

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ from Intra_country_functions import *
 from Inter_Country import inter_country
 from Inter_Continent import inter_continent
 from Inter_Region import inter_region
+
 # Default values for configurable parameters
 row_dropna_threshold_factor = 0.9 # The relation between the number of non empty cells to the total number of cells in a row
 
 # Clean up log files from previous runs
-log_file_paths = ['country_data_anlyze_and_process_log.txt']
-for project_log_file in log_file_paths:
+log_file_list = ['country_data_anlyze_and_process_log.txt', 'intra_country_functions_log.txt']
+for project_log_file in log_file_list:
     if os.path.exists(project_log_file):
         os.remove(project_log_file)
+
 
 # Configure the Streamlit environment of MyCountry App and run it
 set_st_bg('streamlit_countries_background.jpg')
@@ -26,23 +28,20 @@ analysis_type, target_entity = st_user_select_analysis(country_geo_data['countri
 # Run the analysis based on the user's selection
 if analysis_type == 'intra_country':
     country_row = country_data[country_data.index == target_entity]
-    run_intra_country_analysis(country_row, country_column_data['all'])
+    run_intra_country_analysis(country_row, country_column_data['all'] )
 
+if analysis_type == 'inter_country':
+    figs = inter_country(country_data, target_entity, compareTo='World')
+    for fig in figs:
+        st.pyplot(fig)
 
-# To be integrated into the manage_app_interface - Temporarily here for inspection
-#get_country_id_data(country_data.loc[['Canada']],country_all_columns)
+if analysis_type == 'inter_continent':
+    figs= inter_continent(country_data)
+    for fig in figs:
+        st.pyplot(fig)
 
+if analysis_type == 'inter_continent':
+    figs= inter_region(country_data)
+    for fig in figs:
+        st.pyplot(fig)
 
-MyCountry = st.selectbox('Select Country',country_data.index)
-figs = inter_country(country_data,MyCountry,compareTo='World')
-for fig in figs:
-    st.pyplot(fig)
-
-figs= inter_continent(country_data)
-for fig in figs:
-    st.pyplot(fig)
-
-
-figs= inter_region(country_data)
-for fig in figs:
-    st.pyplot(fig)
