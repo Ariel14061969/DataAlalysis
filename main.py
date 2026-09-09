3bd543461b15115f7a398c2fd856005437139415
ariel rubanenko <ariel@algonomi.com>
2026-09-09 18:08:40 +0300

    Add explanations for user

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from libs_and_modules import *
 from config_app_env import *
 from manage_app_interface import *
 from Intra_country_functions import *
-from Inter_Country import inter_country
-from Inter_Continent import inter_continent
-from Inter_Region import inter_region
+from Inter_Country import *
+from Inter_Continent import *
+from Inter_Region import *
 from Correlation_Matrix import *
+from Units_Txt import *
 
 # Default values for configurable parameters
 row_dropna_threshold_factor = 0.9 # The relation between the number of non empty cells to the total number of cells in a row
@@ -26,11 +27,18 @@ config_st_page()
 country_data, country_column_data, country_columns_nan_percentage, country_geo_data = st_ui_start(row_dropna_threshold_factor)
 analysis_type, target_entity = st_user_select_analysis(country_geo_data['countries'],country_geo_data['continents'], country_geo_data['regions'] )
 
+# This prints  the variables explanations on the screen
+char_exp = units_txt()
+st.dataframe(char_exp)
+
 # Run the analysis based on the user's selection
+
 if analysis_type == 'intra_country':
     country_row = country_data[country_data.index == target_entity]
     run_intra_country_analysis(country_row, country_column_data['all'] )
 
+# each of the inter... functions return figures handles to be displayed in streamlit.
+# inside the functions, we do not call streamlit graphics!
 if analysis_type == 'inter_country':
     figs = inter_country(country_data, target_entity, compareTo='World')
     for fig in figs:
